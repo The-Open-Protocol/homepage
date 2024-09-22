b0604fadd18af75cadf387fc2c7d376510004460
theodencrypto <hernandeztheoden@gmail.com>
2024-09-22 20:08:33 +0700

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), alpinejs()],
+  base: '/astro-tailwind-mambaui-template',
+  build: {
+    assets: 'astro'
+  }
+});
